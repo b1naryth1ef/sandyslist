772b83d61f76446213d4e060557c9dd234c050d8
B1naryTh1ef <b1naryth1ef@gmail.com>
2012-11-02 00:59:56 -0500

    Is it Gunicorn?

--- app.py
+++ app.py
@@ -165,4 +165,4 @@ def internals(route=None):
         return redirect('/find')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=os.getenv('PORT', 5000))
